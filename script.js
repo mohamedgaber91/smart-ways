4192a2d17c646485ca4b444791cf65e1999ce1b8
mohamed <2000gaber2000@gmail.com>
2024-10-22 12:10:58 +0300

    edit

--- script.js
+++ script.js
@@ -98,5 +98,17 @@ accordionHeaders.forEach(header => {
 
 
 
+const quantityInput = document.getElementById('quantity');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
 
+incrementButton.addEventListener('click', () => {
+    quantityInput.value = parseInt(quantityInput.value) + 1;
+});
+
+decrementButton.addEventListener('click', () => {
+    if (quantityInput.value > 1) {
+        quantityInput.value = parseInt(quantityInput.value) - 1;
+    }
+});
 
